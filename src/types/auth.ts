efc7cab94f6d0283c27e3926b18f234fc249ee2e
Nii1214 <nii.tomoyuki1214@gmail.com>
2026-01-25 13:43:08 +0900

    feat: ユーザー登録用の型定義を追加

---
+++ src/types/auth.ts
@@ -0,0 +1,17 @@
+// ドメインエンティティ(アプリ内ユーザー情報)
+export type User = {
+    id: string,
+    email: string,
+    createdAt: Date;
+};
+
+// 入力データ
+export type SignupInput = {
+    email: string;
+    password: string;
+};
+
+// 出力データ(Resultパターン)
+export type SignupResult = 
+    | { success: true; user: User}
+    | { success: false; error: string};

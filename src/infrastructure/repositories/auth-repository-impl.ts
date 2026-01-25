a487055e276515795a0adbf4069f8a8fad64ced2
Nii1214 <nii.tomoyuki1214@gmail.com>
2026-01-25 15:16:28 +0900

    feat(infrastructure): AuthRepositoryImplの実装を追加

---
+++ src/infrastructure/repositories/auth-repository-impl.ts
@@ -0,0 +1,34 @@
+import { AuthRepository } from "@/src/domain/repositories/auth-repository";
+import { createClient } from "@/src/lib/supabase/server";
+import { SignupInput, User } from "@/src/types/auth";
+
+export class AuthRepositoryImpl implements AuthRepository {
+    async signup(input: SignupInput): Promise<User> {
+        const supabase = await createClient();
+
+        const  {data, error} = await supabase.auth.signUp({
+            email: input.email,
+            password: input.password,
+        });
+
+        // エラーチェック
+        if(error) {
+            throw new Error(`Signup failed:${error.message}`);
+        }
+        // nullチェック
+        if(!data.user) {
+            throw new Error('ユーザー作成に失敗しました');
+        }
+
+        return {
+            id: data.user.id,
+            email: data.user.email!,
+            createdAt: new Date(data.user.created_at),
+        };
+    }
+
+    async findByEmail(email: string) : Promise<User | null> {
+        // カスタムロジックが必要な場合のみ追加
+        return null;
+    }
+}

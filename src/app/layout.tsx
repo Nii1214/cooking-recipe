a5882c4bec3fe5e8f6c921e8e365f3e96c15e8d4
Nii1214 <nii.tomoyuki1214@gmail.com>
2026-01-25 11:12:11 +0900

    feat: HeaderComponentを作成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "../components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="jp">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 `}
       >
-        {children}
+      <Header/>
+      <main>{children}</main>
       </body>
     </html>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+`use client`
+export default function Header() {
+    return (
+        <>
+            <header className="h-16 flex items-center justify-between bg-white px-6">
+                <div className="flex items-center gap-8">
+                    <span className="font-bold text-xl tracking-tight shrink-0">Cooking Recipe</span>
+                    <input 
+                        type="text"
+                        placeholder="なにつくる？"
+                        className="w-full bg-gray-50 border border-gray-200 rounded-full py-2 px-4 text-sm focus:outline-none focus:ring-2 focus:ring-orange-200 transition-all"
+                    />
+                </div>
+                <div className="flex items-center gap-4">
+                    <button className="px-4 py-2 text-sm font-bold bg-orange-600 hover:bg-orange-300 rounded-full text-sm font-medium text-white transition-colors">ログイン</button>
+                </div>
+            </header>
+        </>
+    )
+}

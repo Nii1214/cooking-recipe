c70cbc1c1d2cf34bdf9d880af0c08cf4a0bfc805
Nii1214 <nii.tomoyuki1214@gmail.com>
2026-02-09 14:41:43 +0900

    refactor: signupActionの不要なimportを削除し記法を統一

---
+++ src/app/(auth)/signup/signup.action.ts
@@ -0,0 +1,34 @@
+'use server';
+import { SignupResult } from "@/types/auth";
+import { redirect } from "next/navigation";
+import { getAuthErrorMessage } from "@/infrastructure/utils/auth-error-handler";
+import { DIContainer } from "@/lib/di-container";
+
+export async function signupAction(
+    _prevState: SignupResult | null,
+    formData: FormData
+):Promise<SignupResult> {
+
+    const email = formData.get('email') as string | null;
+    const password = formData.get('password') as string | null;
+
+    // 依存性注入
+    const useCase = DIContainer.getSignupUseCase();
+
+    try{
+        const result = await useCase.execute({ 
+            email: email ?? '', 
+            password: password ?? '' 
+        });
+        
+        if(result.success) {
+            redirect('/signup/verify-email');
+        }
+        return result;
+    }catch(error) {
+        return {
+            success: false,
+            error: getAuthErrorMessage(error),
+        };
+    }
+}
